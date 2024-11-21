8b8acae2326149f13974e6082ed1b6b4d273f99e
Dave-314land <dave.l.piland@gmail.com>
2024-11-21 16:03:08 -0500

    added mongodb connection with env variables

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+
+const dbURL = process.env.DB_URL;
+
+mongoose
+    .connect(dbURL)
+    .then(() => 
+        {console.log("mongodb connected!")}
+    ).catch((error) => {
+        console.log(error)
+    });
 
 const app = express()
 const port = 3000
